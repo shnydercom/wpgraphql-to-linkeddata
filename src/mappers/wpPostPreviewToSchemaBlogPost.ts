8d6ef727fce87e4f113e63f91d53b3420bfe1629
shnyder <ajs.private@gmail.com>
2021-02-16 14:51:33 +0100

    strict type checking added

--- src/mappers/wpPostPreviewToSchemaBlogPost.ts
+++ src/mappers/wpPostPreviewToSchemaBlogPost.ts
@@ -7,26 +7,41 @@ import { BlogPosting } from "schema-dts";
  * @param wpBaseURL the base domain of your wordpress installation. Used to add the slug
  */
 export function mapWpPostPreviewToSchemaBlogPost(
-    input: PostPreviewFragment,
-    wpBaseURL?: string
-): BlogPosting {
-	let previewNode = input.preview.node;
-	let featuredImgNode = previewNode.featuredImage.node;
-	let thumbnailUrl = featuredImgNode.srcSet
-		.split(" ")
+	input: PostPreviewFragment,
+	wpBaseURL?: string
+): BlogPosting | null {
+	let previewNode = input!.preview!.node;
+	let featuredImgNode = previewNode!.featuredImage!.node;
+	let thumbnailUrl = featuredImgNode!
+		.srcSet!.split(" ")
 		.find((val) => val.startsWith("http://"));
+	if (!previewNode || !thumbnailUrl) return null;
 	let output: BlogPosting = {
-        "@type": "BlogPosting",
-        ...() => {wpBaseURL ? {"@id":`${wpBaseURL}/${input.slug}`} : undefined},
-		name: input.title,
-		abstract: previewNode.excerpt,
-		image: {
-			"@type": "ImageObject",
-			description: featuredImgNode.description,
-            name: featuredImgNode.altText,
-            contentUrl: featuredImgNode.sourceUrl,
-            thumbnailUrl
-		},
+		"@type": "BlogPosting",
+		...(wpBaseURL && {
+			"@id": `${wpBaseURL}/${input.slug}`,
+		}),
+		...(input.title && {
+			name: input.title,
+		}),
+		...(previewNode.excerpt && {
+			abstract: previewNode.excerpt,
+		}),
+		...(featuredImgNode && {
+			image: {
+				"@type": "ImageObject",
+				...(featuredImgNode.description && {
+					description: featuredImgNode.description,
+				}),
+				...(featuredImgNode.altText && {
+					name: featuredImgNode.altText,
+				}),
+				...(featuredImgNode.sourceUrl && {
+					contentUrl: featuredImgNode.sourceUrl,
+				}),
+				thumbnailUrl,
+			},
+		}),
 		thumbnailUrl,
 	};
 	return output;
